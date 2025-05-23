088f0ee3804ed08be9d87dafd509ac2787ff2e02
Haima <seethasri001@gmail.com>
2025-05-23 22:43:52 +0530

    mysql installation

--- 10.install-command.sh
+++ 10.install-command.sh
@@ -5,9 +5,10 @@ USERID=$(id  -u)
 if [ $USEID -ne 0 ]
 then
      echo "Please run this script with Root access"
+     exit 1 #give other than 0 to upto 127
   else
       echo "You are running with Root access"
   fi
 
   dnf install mysql -y
-  
+
